112081cc4e27614fa33eb31f2d01361c1705e487
prajwalkk22 <prajwal11kk22@gmail.com>
2025-04-07 21:01:27 +0530

    chore: setup MongoDB connection using Mongoose with environment variables

--- vidtube/src/index.js
+++ vidtube/src/index.js
@@ -1,1 +1,22 @@
-console.log("Hello from backend journey")
+import dotenv from "dotenv"
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+dotenv.config({
+    path:"./.env"
+})
+
+
+
+
+const PORT = process.env.PORT || 8000;
+
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Mong db is connected http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed:", err);
+  });
